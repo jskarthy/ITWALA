f8d4619c31b8028749901390efc4b49d220bec7e
jskarthy <jskarthy@gmail.com>
2016-05-03 00:50:16 +0530

    adding missing references

--- branches/Feb/ITWALA/App_Start/App_Start/OAuthConfig.cs
+++ branches/Feb/ITWALA/App_Start/App_Start/OAuthConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using ITWALA.Models;
+﻿using System.Collections.Generic;
 using Microsoft.Web.WebPages.OAuth;
 
 namespace ITWALA.App_Start
